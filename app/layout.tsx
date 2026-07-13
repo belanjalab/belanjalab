c1e89747d17e6037eccfe74be884c06e1c48407a
dikjulyan-spec <dik.julyan@gmail.com>
2026-07-13 16:56:51 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "BelanjaLab",
+  description:
+    "Platform rekomendasi dan perbandingan produk untuk belanja lebih cerdas.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
